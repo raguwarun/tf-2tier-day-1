f8209871a19c5ef4d265dbf6a30505dc13c72918
raas1001 <45093832+raguwarun@users.noreply.github.com>
2025-04-01 16:55:54 +0530

    Update ec2-resources.tf

--- ec2-resources.tf
+++ ec2-resources.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "two-tier-web-1" {
   subnet_id = aws_subnet.two-tier-pub-sub-1.id
-  ami ="ami-071226ecf16aa7d96"
+  ami ="ami-076c6dbba59aa92e6"
   instance_type = "t2.micro"
   security_groups = [aws_security_group.two-tier-ec2-sg.id]
   key_name = "two-tier-key"
@@ -20,7 +20,7 @@ EOF
 
 resource "aws_instance" "two-tier-web-2" {
   subnet_id = aws_subnet.two-tier-pub-sub-2.id
-  ami="ami-071226ecf16aa7d96"
+  ami="ami-076c6dbba59aa92e6"
   instance_type = "t2.micro"
   security_groups = [aws_security_group.two-tier-ec2-sg.id]
   key_name = "two-tier-key"
